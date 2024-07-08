5ecf27f5ba2f324a4c50845d2b43428318b5a9a4
nametake <nametake.kyarabuki@gmail.com>
2024-07-09 02:07:18 +0900

    Impl Preorder

--- nopkgvarerrorf.go
+++ nopkgvarerrorf.go
@@ -24,14 +24,22 @@ func run(pass *analysis.Pass) (any, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.ValueSpec)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+		if vspec, ok := n.(*ast.ValueSpec); ok {
+			for _, value := range vspec.Values {
+				if call, ok := value.(*ast.CallExpr); ok {
+					if sel, ok := call.Fun.(*ast.SelectorExpr); ok {
+						if _, ok := sel.X.(*ast.Ident); ok && sel.Sel.Name == "Errorf" {
+							pass.Reportf(n.Pos(), "do not use Errorf at the package level")
+						}
+					}
+					if ident, ok := call.Fun.(*ast.Ident); ok && ident.Name == "Errorf" {
+						pass.Reportf(n.Pos(), "do not use Errorf at the package level")
+					}
+				}
 			}
 		}
 	})
