e20de7daf178c0852b2a427e255292169a13a5f5
nametake <nametake.kyarabuki@gmail.com>
2024-07-09 02:01:44 +0900

    Added testcase

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -7,3 +7,14 @@ var ErrNotFound = fmt.Errorf("not found") // want "do not use Errorf at the pack
 func f() error {
 	return fmt.Errorf("not found")
 }
+
+func g() {
+	err := fmt.Errorf("not found")
+	fmt.Println(err)
+}
+
+var ErrNotFound2 = Errorf() // want "do not use Errorf at the package level"
+
+func Errorf() error {
+	return fmt.Errorf("not found")
+}
